4d592151b88d1b8a39b4606de91cde1c7358f3dd
yaocl <yaochenglindlut@gmail.com>
2015-12-14 11:06:37 +0800

    first kick off

---
+++ cn.evosoft.eclipse.product/src/cn/evosoft/eclipse/product/custom/ExamApplication.java
@@ -0,0 +1,20 @@
+package cn.evosoft.eclipse.product.custom;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class ExamApplication implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
